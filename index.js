6641eb0436ad1d3e196a72b5db8a961451c7c419
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-25 14:50:34 +0300

    add

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+  const titleCased = () => {
+    return tutorials.map(title => {
+      return title.split(' ').map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
+    });
+  }
+
